fba5d12a14f1f97b45d208bc94186ee6f3981f21
Aritra <aritra.03@proton.me>
2026-07-25 15:58:01 +0530

    Updated invoice template and invoice controller now creates context from database data

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -1,19 +1,39 @@
 import ApiError from "../utils/ApiError.js";
+import orderModel from "../models/orders.model.js";
 
 export async function renderInvoice(req, res, next) {
     try {
-        const { name, price } = req.query;
+        const { orderId } = req.query;
+        if (!orderId) throw new ApiError(400, "Order ID is required to generate Invoice !");
 
-        const basePrice = parseFloat(price || "100");
-        const increasedPrice = basePrice + 50;
-        const totalPrice = basePrice + increasedPrice;
+        //find the order
+        const ord = await orderModel.findById(orderId);
+        if (!ord) throw new ApiError(404, "No valid orders found with this order ID !");
+
+        if (ord.status === "PENDING" || ord.status === "FAILED")
+            throw new ApiError(400, "Order is either pending or failed completely ! Cannot generated invoice for pending or failed orders !");
 
         const context = {
-            name: name || "Client Name",
-            price: basePrice,
-            increasedPrice,
-            totalPrice
-        };
+            name: ord.nameOnOrder,
+            email: ord.email,
+            items: ord.items.map(function (item) {
+                const base = item.sellingPrice * item.quantity;
+                const tax = base * (item.taxAtTimeOfPurchase / 100);
+
+                return {
+                    name: item.itemName,
+                    SKU: item.itemSKU,
+                    quantity: item.quantity,
+                    price: item.sellingPrice,
+                    taxApplicable: item.taxAtTimeOfPurchase,
+                    lineTotal: Math.round((base + tax) * 100) / 100
+                }
+            }),
+            sub_total: ord.subtotal,
+            total: ord.total,
+            invoiceId: ord.invoiceId,
+            rzp_order_id: ord.razorpayOrderId
+        }
 
         return res.render("invoice", context);
     }
