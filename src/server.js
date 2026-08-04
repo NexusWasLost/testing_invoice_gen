73941a505727acdf10f45d681e39d148dd03e6a5
Aritra <aritra.2003k@gmail.com>
2026-08-05 00:36:04 +0530

    Fix: Fixed imcomplete CORS origin setup

--- src/server.js
+++ src/server.js
@@ -17,7 +17,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(cors({
+    origin: ["https://pixelstore.pages.dev"],
+    methods: ["GET", "POST", "OPTIONS"]
+}));
 
 app.engine("hbs", engine({ extname: ".hbs", layoutsDir: false, defaultLayout: false }));
 app.set("view engine", "hbs");
